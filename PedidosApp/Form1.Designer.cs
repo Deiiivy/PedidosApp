2ca8279acc5a0bc342484ceb3932b321cef5c984
Deiiivy <deivypr28@gmail.com>
2025-04-23 11:59:15 -0500

    se termina la interfaz

--- PedidosApp/Form1.Designer.cs
+++ PedidosApp/Form1.Designer.cs
@@ -28,13 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtCliente = new System.Windows.Forms.TextBox();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.chkUrgente = new System.Windows.Forms.CheckBox();
+            this.nudPeso = new System.Windows.Forms.NumericUpDown();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.nudDistancia = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDistancia)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtCliente
+            // 
+            this.txtCliente.Location = new System.Drawing.Point(186, 65);
+            this.txtCliente.Name = "txtCliente";
+            this.txtCliente.Size = new System.Drawing.Size(121, 20);
+            this.txtCliente.TabIndex = 0;
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(186, 121);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 21);
+            this.cmbProducto.TabIndex = 1;
+            // 
+            // chkUrgente
+            // 
+            this.chkUrgente.AutoSize = true;
+            this.chkUrgente.Location = new System.Drawing.Point(186, 173);
+            this.chkUrgente.Name = "chkUrgente";
+            this.chkUrgente.Size = new System.Drawing.Size(80, 17);
+            this.chkUrgente.TabIndex = 2;
+            this.chkUrgente.Text = "checkBox1";
+            this.chkUrgente.UseVisualStyleBackColor = true;
+            // 
+            // nudPeso
+            // 
+            this.nudPeso.Location = new System.Drawing.Point(186, 221);
+            this.nudPeso.Name = "nudPeso";
+            this.nudPeso.Size = new System.Drawing.Size(120, 20);
+            this.nudPeso.TabIndex = 3;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(202, 309);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(458, 72);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(58, 13);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // nudDistancia
+            // 
+            this.nudDistancia.Location = new System.Drawing.Point(186, 262);
+            this.nudDistancia.Name = "nudDistancia";
+            this.nudDistancia.Size = new System.Drawing.Size(120, 20);
+            this.nudDistancia.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(82, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Nombre cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(82, 129);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "tipo producto";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(100, 173);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Urgente?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(83, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Peso Producto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(87, 269);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Distancia(KM)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nudDistancia);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.nudPeso);
+            this.Controls.Add(this.chkUrgente);
+            this.Controls.Add(this.cmbProducto);
+            this.Controls.Add(this.txtCliente);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudPeso)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDistancia)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtCliente;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.CheckBox chkUrgente;
+        private System.Windows.Forms.NumericUpDown nudPeso;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.NumericUpDown nudDistancia;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- PedidosApp/Form1.cs
+++ PedidosApp/Form1.cs
@@ -15,6 +15,30 @@ namespace PedidosApp
         public Form1()
         {
             InitializeComponent();
+            cmbProducto.Items.AddRange(new string[] { "tecnología", "accesorio", "componente" });
+            cmbProducto.SelectedIndex = 0;
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string cliente = txtCliente.Text;
+                string producto = cmbProducto.SelectedItem.ToString();
+                bool urgente = chkUrgente.Checked;
+                double peso = Convert.ToDouble(nudPeso.Value);
+                int distancia = Convert.ToInt32(nudDistancia.Value);
+
+                Pedido pedido = new Pedido(cliente, producto, urgente, peso, distancia);
+                RegistroPedidos.Instancia.AgregarPedido(pedido);
+
+                lblResultado.Text = $"Entrega: {pedido.MetodoEntrega.TipoEntrega()}\n" +
+                                    $"Costo: ${pedido.ObtenerCosto():0.00}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 }
